6f4f7e2e2e2ed28367c4507c3002715a89e2933c
BastiaanOlij <mux213@gmail.com>
2016-03-20 15:12:36 +1100

    Use black instead of white for transparent background

--- source/engine.c
+++ source/engine.c
@@ -372,7 +372,7 @@ void addTrees(const char *pModelPath) {
       glViewport(0,0,1024,1024);
 
       // clear our texture
-      glClearColor(1.0, 1.0, 1.0, 0.0);
+      glClearColor(0.0, 0.0, 0.0, 0.0);
       glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);      
 
       // setup our lightsource
