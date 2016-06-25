29d5ad91e0ebb92fad7d7d95af7d5d21cf1f0620
BastiaanOlij <mux213@gmail.com>
2016-06-25 11:18:15 +1000

    Discard fragment if it doesn't effect our scene

--- resources/Shaders/geopointlight.fs
+++ resources/Shaders/geopointlight.fs
@@ -5,6 +5,7 @@
 // info about our light
 uniform vec3      lightPos;                         // position of our light after view matrix was applied
 uniform vec3      lightCol;                         // color of the light of our sun
+uniform float     radius = 100.0;                   // maximum distance to light at which we still illuminate things
 uniform float     attConstant = 0.0;
 uniform float     attLinear = 0.0;
 uniform float     attExp = 0.0;
@@ -22,11 +23,12 @@ void main() {
 #ifdef barreldist
   vec2 T = barreldist(V, 1);
   if ((T.x < 0.0) || (T.x > 1.0) || (T.y < 0.0) || (T.y > 1.0)) {
-    fragcolor = vec4(0.0, 0.0, 0.0, 1.0);
+    discard;
   } else {
 #else
   vec2 T = (V + 1.0) / 2.0;
 #endif
+
     vec4 V = vec4((texture(worldPos, T).xyz - 0.5) * posScale, 1.0);
     vec3 difColor = texture(diffuse, T).rgb;
     vec4 specColor = texture(specular, T);
@@ -38,6 +40,12 @@ void main() {
     // Get the normalized directional vector between our surface position and our light position
     vec3  LmV = lightPos - V.xyz;
     float Dist = length(LmV);
+    if (Dist > radius) {
+      // we're too far away from the light for it to effect us
+      discard;
+    };
+
+    // get our light vector
     vec3  L = normalize(LmV);
     float NdotL = max(0.0, dot(N, L));
     difColor = difColor * NdotL * lightCol * shadowFactor;
@@ -63,11 +71,6 @@ void main() {
     } else {
       fragcolor = vec4((difColor + specColor.rgb) / attenuation, 1.0);
     };
-
-    // just for testing
-//    if (R <= 2.0) {
-//      fragcolor = fragcolor + vec4(lightCol, 1.0);
-//    };
 #ifdef barreldist
   }
 #endif
